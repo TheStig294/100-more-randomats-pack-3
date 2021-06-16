beb33e3560e9e97d1f1206a35f5bfbc22eec847b
The Stig <TheStig294@users.noreply.github.com>
2021-06-17 02:37:10 +0930

    Cleanup and added comments

--- lua/randomat2/events/afk.lua
+++ lua/randomat2/events/afk.lua
@@ -1,35 +1,36 @@
 local EVENT = {}
-
 EVENT.Title = "AFK = Dead"
 EVENT.Description = "Being AFK kills you"
 EVENT.id = "afk"
 
 function EVENT:Begin()
-	hook.Add("Think", "AFKKillThink", function()
-		for i, ply in pairs(self:GetAlivePlayers()) do
-			timer.Simple(0.1, function()
-				if ply:GetNWBool("Minimized", false) and ply:GetNWBool("MinimizedKill", false) == false then
-					ply:Kill()
-					ply:SetNWBool("MinimizedKill", true)
-					self:SmallNotify(ply:Nick() .. " went AFK")
-				end
-			end)
-		end
-	end)
-	
-	hook.Add("PlayerSpawn", "AFKKillRespawnCheck", function(ply, transition)
-		timer.Simple(0.1, function()
-			ply:SetNWBool("MinimizedKill", false)
-		end)
-	end)
-end
+    -- Set the flag that lets a player be killed by being AFK on all players
+    for i, ply in pairs(self:GetPlayers()) do
+        ply:SetNWBool("MinimizedKillable", true)
+    end
 
-function EVENT:End()
-	for i, ply in pairs(player.GetAll()) do
-		ply:SetNWBool("MinimizedKill", false)
-	end
-	hook.Remove("Think", "AFKKillThink")
-	hook.Remove("PlayerSpawn", "AFKKillRespawnCheck")
+    self:AddHook("Think", function()
+        for i, ply in pairs(self:GetAlivePlayers()) do
+            timer.Simple(0.1, function()
+                -- If a player is 'Minimised' (from another mod), and not already killed in this way, 
+                if ply:GetNWBool("Minimized", false) and ply:GetNWBool("MinimizedKillable", false) then
+                    -- Kill them
+                    ply:Kill()
+                    -- Prevent trying to kill them multiple times
+                    ply:SetNWBool("MinimizedKillable", false)
+                    -- Display a notification to everyone explaining why they died
+                    self:SmallNotify(ply:Nick() .. " went AFK")
+                end
+            end)
+        end
+    end)
+
+    -- Lets a player be killed again by being AFK on respawning
+    self:AddHook("PlayerSpawn", function(ply, transition)
+        timer.Simple(0.1, function()
+            ply:SetNWBool("MinimizedKillable", true)
+        end)
+    end)
 end
 
 Randomat:register(EVENT)
