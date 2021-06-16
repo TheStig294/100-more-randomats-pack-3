40e62ab834e3f725b17da4a41f9f451904c73526
The Stig <TheStig294@users.noreply.github.com>
2021-06-17 02:08:48 +0930

    Added comments

--- lua/randomat2/cl_events/cl_thirdperson.lua
+++ lua/randomat2/cl_events/cl_thirdperson.lua
@@ -1,30 +1,42 @@
 local thirdPersonWasOn = false
 
 net.Receive("ThirdPersonRandomat", function()
-	thirdPersonWasOn = GetConVar("thirdperson_etp"):GetBool()
-	
-	LocalPlayer():ConCommand("thirdperson_etp 1")
-	
-	hook.Add("PlayerBindPress", "DisableThirdpersonToggle", function(ply, bind, pressed)
-		if ( string.find( bind, "thirdperson_enhanced_toggle" ) ) then
-			ply:PrintMessage(HUD_PRINTCENTER, "Third person toggle is disabled")
-			return true
-		end
-	end)
-	
-	hook.Add("Think", "ThirdPersonRandomatFailsafe", function()
-		if not GetConVar("thirdperson_etp"):GetBool() then
-			GetConVar("thirdperson_etp"):SetBool(true)
-		end
-	end)
+    -- Get whether the player was already in third person
+    thirdPersonWasOn = GetConVar("thirdperson_etp"):GetBool()
+    -- Force the player into third person
+    LocalPlayer():ConCommand("thirdperson_etp 1")
+
+    -- When the player presses a keybind,
+    hook.Add("PlayerBindPress", "DisableThirdpersonToggle", function(ply, bind, pressed)
+        -- And that key is bound to toggling the third person perspective,
+        if (string.find(bind, "thirdperson_enhanced_toggle")) then
+            -- Tell the player they cannot toggle third person
+            ply:PrintMessage(HUD_PRINTCENTER, "Third person toggle is disabled")
+            -- And prevent them from toggling it from this keybind
+
+            return true
+        end
+    end)
+
+    -- Constantly check,
+    hook.Add("Think", "ThirdPersonRandomatFailsafe", function()
+        -- If ever the player is somehow able to go back to first person,
+        if not GetConVar("thirdperson_etp"):GetBool() then
+            -- Set them back to third person
+            GetConVar("thirdperson_etp"):SetBool(true)
+        end
+    end)
 end)
 
 net.Receive("ThirdPersonRandomatEnd", function()
-	hook.Remove("PlayerBindPress", "DisableThirdpersonToggle")
-	hook.Remove("Think", "ThirdPersonRandomatFailsafe")
-	
-	if thirdPersonWasOn == false then
-		LocalPlayer():ConCommand("thirdperson_etp 0")
-	end
-	
+    -- Let the player press their third person toggle keybind again
+    hook.Remove("PlayerBindPress", "DisableThirdpersonToggle")
+    -- Stop forcing the player to third person
+    hook.Remove("Think", "ThirdPersonRandomatFailsafe")
+
+    -- If the player was not already in third-person,
+    if thirdPersonWasOn == false then
+        -- Set the player back to first-person
+        LocalPlayer():ConCommand("thirdperson_etp 0")
+    end
 end)
