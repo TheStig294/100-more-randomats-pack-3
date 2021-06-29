5a74ce05bd4b0e547ebc821246f5b673953e8350
The Stig <TheStig294@users.noreply.github.com>
2021-06-29 23:41:19 +0930

    Pruned comments

--- lua/randomat2/events/buyemall.lua
+++ lua/randomat2/events/buyemall.lua
@@ -8,10 +8,8 @@ local notTriggered = true
 function EVENT:Begin()
     -- This randomat can only trigger once per map
     notTriggered = false
-    -- Getting the stats recorded by 'TTT Total Statistics'
     local data = file.Read("ttt/ttt_total_statistics/stats.txt", "DATA")
     local stats = util.JSONToTable(data)
-    -- Getting useable lists of all the buyable detective and traitor weapons
     -- Functionality of GetDetectiveBuyable() and GetTraitorBuyable() can be found in stig_randomat_base_functions.lua and stig_randomat_client_functions.lua
     local detectiveBuyableKeyed = GetDetectiveBuyable()
     local traitorBuyableKeyed = GetTraitorBuyable()
@@ -20,7 +18,6 @@ function EVENT:Begin()
     -- Get the no. of detective/traitor weapons, used for the percentage complete printed to chat
     local detectiveBuyableCount = #detectiveBuyable
     local traitorBuyableCount = #traitorBuyable
-    -- Finally, reset the table of players that have bought every weapon at least once
     local boughtEmAllPlayers = {}
 
     for i, ply in pairs(self:GetPlayers()) do
@@ -50,32 +47,26 @@ function EVENT:Begin()
             boughtAllTraitor = true
         end
 
-        -- If the player has bought every detective and traitor item at least once,
         if boughtAllDetective and boughtAllTraitor then
-            -- Add them to the table of players who've bought everything and print a message to chat
             table.insert(boughtEmAllPlayers, ply)
             ply:ChatPrint("==CONGRATS! YOU BOUGHT 'EM ALL!==\nYou get to choose randomats at the start of each round!")
 
             timer.Simple(5, function()
                 Randomat:SmallNotify("Someone has bought 'em all!")
             end)
-            -- If the player hasn't bought every traitor weapon,
         elseif boughtAllDetective and not boughtAllTraitor then
-            -- Double their health and print a message to chat explaining why,
             ply:SetMaxHealth(ply:GetMaxHealth() * 2)
             ply:SetHealth(ply:GetMaxHealth())
             ply:ChatPrint("Congrats! You have bought every detective item, your health has been doubled!")
-            -- And give them a traitor weapon they haven't bought before
+            -- Give them a traitor weapon they haven't bought before
             -- PrintToGive() takes a weapon's print name, a player and gives them the weapon, found in stig_randomat_base_functions.lua
             PrintToGive(traitorBuyable[math.random(#traitorBuyable)], ply)
             ply:ChatPrint("However, you haven't bought every traitor item at least once. \n(" .. math.Round((#traitorBought / traitorBuyableCount) * 100) .. "% complete)\nBuy them all for the REAL reward...")
         elseif not boughtAllDetective and boughtAllTraitor then
-            -- If the player hasn't bought every detective weapon,
-            -- Double their health and print a message to chat explaining why,
             ply:SetMaxHealth(ply:GetMaxHealth() * 2)
             ply:SetHealth(ply:GetMaxHealth())
             ply:ChatPrint("Congrats! You have bought every traitor item, your health has been doubled!")
-            -- And give them a traitor weapon they haven't bought before
+            -- Give them a traitor weapon they haven't bought before
             PrintToGive(detectiveBuyable[math.random(#detectiveBuyable)], ply)
             ply:ChatPrint("You haven't bought every detective item at least once. \n(" .. math.Round((#detectiveBought / detectiveBuyableCount) * 100) .. "% complete)\nBuy them all for the REAL reward...")
         else
@@ -86,18 +77,14 @@ function EVENT:Begin()
         end
     end
 
-    -- Getting the server's current settings that may be changed, so they can be reset
     local autoRandomat = GetConVar("ttt_randomat_auto"):GetBool()
     local choices = GetConVar("randomat_choose_choices"):GetInt()
     local vote = GetConVar("randomat_choose_vote"):GetBool()
 
-    -- If at least one player has bought every detective/traitor item,
     if table.IsEmpty(boughtEmAllPlayers) == false then
-        -- Turn off auto randomat,
         GetConVar("ttt_randomat_auto"):SetBool(false)
-        -- Set the number of randomats the player has to choose from to 5,
         GetConVar("randomat_choose_choices"):SetInt(5)
-        -- And only allow the player who the randomat triggers off of to choose randomats
+        -- Only allow the player who the randomat triggers off of to choose randomats
         GetConVar("randomat_choose_vote"):SetBool(false)
 
         -- At the start of every round, for the rest of the current map, a random player that bought every weapon gets to choose the randomat for that round
@@ -105,7 +92,7 @@ function EVENT:Begin()
             Randomat:SilentTriggerEvent("choose", table.Random(boughtEmAllPlayers))
         end)
 
-        -- Once the map is changing or the server is being shut down, reset all changed convars
+        -- Once the map is changing or the server is being shut down, all convars are reset
         hook.Add("ShutDown", "BoughtEmAllConVarReset", function()
             GetConVar("ttt_randomat_auto"):SetBool(autoRandomat)
             GetConVar("randomat_choose_choices"):SetInt(choices)
@@ -115,7 +102,7 @@ function EVENT:Begin()
 end
 
 function EVENT:Condition()
-    -- Trigger when this randomat has not triggered before this map, the 'Choose an Event!' randomat exists and is allowed to trigger and 'TTT Total Statistics' is installed
+    -- This event is reliant on 'Choose an Event!' existing and being turned on and 'TTT Total Statistics' being installed
     return notTriggered and Randomat:CanEventRun("choose") and file.Exists("gamemodes/terrortown/entities/entities/ttt_total_statistics/init.lua", "THIRDPARTY")
 end
 
