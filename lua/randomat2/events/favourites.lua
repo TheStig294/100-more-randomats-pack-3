e2c02c3cecf20bd8160b8f0be90c73fad4a10ba9
The Stig <TheStig294@users.noreply.github.com>
2021-07-01 02:43:29 +0930

    Major cleanup and added comments, players that have no bought weapons are now given a radar and body armour instead

--- lua/randomat2/events/favourites.lua
+++ lua/randomat2/events/favourites.lua
@@ -4,33 +4,43 @@ EVENT.Description = "Gives your most bought traitor + detective item, unless the
 EVENT.id = "favourites"
 
 function EVENT:Begin()
+    -- The stats data is recorded from another mod, 'TTT Total Statistics'
     local data = file.Read("ttt/ttt_total_statistics/stats.txt", "DATA")
-
-    if data == nil then
-        timer.Simple(5, function()
-            self:SmallNotify("[TTT] Total Statistics not installed... Randomat can't trigger")
-        end)
-
-        return
-    else
-        stats = util.JSONToTable(data)
-    end
+    local stats = util.JSONToTable(data)
 
     for i, ply in pairs(self:GetAlivePlayers()) do
         timer.Simple(0.1, function()
-            id = ply:SteamID()
-            nickname = ply:Nick()
-            detectiveStats = stats[id]["DetectiveEquipment"]
-            detectiveItemName = table.GetWinningKey(detectiveStats)
-            traitorStats = stats[id]["TraitorEquipment"]
-            traitorItemName = table.GetWinningKey(traitorStats)
+            local id = ply:SteamID()
+            local detectiveStats = stats[id]["DetectiveEquipment"]
+            local traitorStats = stats[id]["TraitorEquipment"]
+            -- Set every player's buy count of the radar and body armour to 0 to prevent these from always being a player's most bought item
+            -- Also effectively sets the player's most bought item to the body armour as a fail-safe if the player has never bought anything before
+            detectiveStats["Radar"] = 0
+            detectiveStats["Body Armor"] = 0
+            traitorStats["Radar"] = 0
+            traitorStats["Body Armor"] = 0
+            -- Gets the print name of their most bought traitor and detective item
+            local detectiveItemName = table.GetWinningKey(detectiveStats)
+            local traitorItemName = table.GetWinningKey(traitorStats)
 
+            -- The most bought item between the detective and traitor equipment is given first
             if detectiveStats[detectiveItemName] >= traitorStats[traitorItemName] then
                 PrintToGive(detectiveItemName, ply)
-                PrintToGive(traitorItemName, ply)
+
+                -- If they're about to be given the body armour a second time, give them the radar instead
+                if detectiveItemName == "Body Armor" and traitorItemName == "Body Armor" then
+                    PrintToGive("Radar", ply)
+                else
+                    PrintToGive(traitorItemName, ply)
+                end
             else
                 PrintToGive(traitorItemName, ply)
-                PrintToGive(detectiveItemName, ply)
+
+                if traitorItemName == "Body Armor" and detectiveItemName == "Body Armor" then
+                    PrintToGive("Radar", ply)
+                else
+                    PrintToGive(detectiveItemName, ply)
+                end
             end
         end)
     end
