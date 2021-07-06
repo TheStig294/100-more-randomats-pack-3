d5db4bed7205e773651d68c7512958d557742646
The Stig <TheStig294@users.noreply.github.com>
2021-07-06 12:34:26 +0930

    Major cleanup and added comments

--- lua/randomat2/events/sharky.lua
+++ lua/randomat2/events/sharky.lua
@@ -4,43 +4,44 @@ EVENT.Description = "Puts someone with their best traitor partner"
 EVENT.id = "sharky"
 
 function EVENT:Begin()
+    -- The stats data is recorded from another mod, 'TTT Total Statistics'
     local data = file.Read("ttt/ttt_total_statistics/stats.txt", "DATA")
+    local stats = util.JSONToTable(data)
+    local chosenTraitor = table.Random(self:GetAlivePlayers())
+    local id = chosenTraitor:SteamID()
+    local nickname = chosenTraitor:Nick()
 
-    if data == nil then
-        timer.Simple(5, function()
-            self:SmallNotify("[TTT] Total Statistics not installed... Randomat can't trigger")
-        end)
-
-        return
-    else
-        stats = util.JSONToTable(data)
-        math.randomseed(os.time())
-    end
-
-    chosenTraitor = table.Random(self:GetAlivePlayers())
-    id = chosenTraitor:SteamID()
-    nickname = chosenTraitor:Nick()
-
+    -- Compatibility with original stats mod, and the one made for Noxx's custom roles
     if IsValid(stats[id]["TraitorPartners"]) ~= nil then
         traitorStats = stats[id]["TraitorPartners"]
     else
         traitorStats = stats[id]["traitorPartners"]
     end
 
-    traitorWinRates = {}
+    local traitorWinRates = {}
 
+    -- Getting the winrates of everyone but the chosen player, while they were partnered with the chosen player
     for i, ply in pairs(self:GetAlivePlayers()) do
         if ply:SteamID() ~= id then
             traitorWinRates[traitorStats[ply:SteamID()]["Wins"] / traitorStats[ply:SteamID()]["Rounds"]] = ply:Nick()
         end
     end
 
-    bestTraitorWinRateTable = table.GetKeys(traitorWinRates)
+    -- Finding the chosen player's best partner
+    local bestTraitorWinRateTable = table.GetKeys(traitorWinRates)
     table.sort(bestTraitorWinRateTable)
-    bestTraitorWinRate = bestTraitorWinRateTable[#bestTraitorWinRateTable]
-    bestTraitorNickname = traitorWinRates[bestTraitorWinRate]
-    traitorCount = 0
+    local bestTraitorWinRate = bestTraitorWinRateTable[#bestTraitorWinRateTable]
+    local bestTraitorNickname = traitorWinRates[bestTraitorWinRate]
+    -- Counting the number of traitors so that number is preserved after the chosen player and their worst partner are made traitors
+    local traitorCount = 0
 
+    for i, ply in pairs(self:GetAlivePlayers()) do
+        if Randomat:IsTraitorTeam(ply) then
+            traitorCount = traitorCount + 1
+        end
+    end
+
+    -- Setting the chosen player and their best partner to be the traitors
     for i, ply in pairs(self:GetAlivePlayers()) do
         if ply:Nick() == nickname or ply:Nick() == bestTraitorNickname then
             self:StripRoleWeapons(ply)
@@ -55,23 +56,34 @@ function EVENT:Begin()
         end
     end
 
+    -- Randomly choosing non-traitors to be the remaining traitors
+    for i, ply in pairs(self:GetAlivePlayers(true)) do
+        if traitorCount > 2 and Randomat:IsTraitorTeam(ply) == false then
+            self:StripRoleWeapons(ply)
+            Randomat:SetRole(ply, ROLE_TRAITOR)
+            ply:SetCredits(GetConVar("ttt_credits_starting"):GetInt())
+            SendFullStateUpdate()
+            traitorCount = traitorCount - 1
+        end
+    end
+
+    -- Displaying the traitors' winrate
     timer.Simple(5, function()
         self:SmallNotify("The traitors have a " .. math.Round(bestTraitorWinRate * 100) .. "% win rate!")
     end)
 end
 
 function EVENT:Condition()
-    -- Awful code that'll be fixed later...
-    traitorCount = 0
+    local traitorCount = 0
 
     for i, ply in pairs(self:GetAlivePlayers()) do
-        if ply:GetRole() == ROLE_TRAITOR or ply:GetRole() == ROLE_ASSASIN or ply:GetRole() == ROLE_HYPNOTIST or ply:GetRole() == ROLE_VAMPIRE then
+        if Randomat:IsTraitorTeam(ply) then
             traitorCount = traitorCount + 1
         end
     end
-    -- Trigger when there are at least 2 traitors, and 'TTT Total Statistics' is installed
+    -- Trigger when there is more than 1 traitor, and 'TTT Total Statistics' is installed
 
-    return traitorCount >= 2 and file.Exists("gamemodes/terrortown/entities/entities/ttt_total_statistics/init.lua", "THIRDPARTY")
+    return traitorCount > 1 and file.Exists("gamemodes/terrortown/entities/entities/ttt_total_statistics/init.lua", "THIRDPARTY")
 end
 
 Randomat:register(EVENT)
