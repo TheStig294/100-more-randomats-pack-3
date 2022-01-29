dff59bb7f379e829e0ac81d3d8f6e156ea13fd53
The Stig <TheStig294@users.noreply.github.com>
2022-01-29 20:11:24 +1030

    Now gives perk bottles properly as passive shop items, calls shop hooks and changed description

--- lua/randomat2/events/drinkup.lua
+++ lua/randomat2/events/drinkup.lua
@@ -1,23 +1,36 @@
-local perks = {"ttt_perk_doubletap", "ttt_perk_juggernog", "ttt_perk_phd", "ttt_perk_speed", "ttt_perk_staminup"}
-
 local EVENT = {}
 EVENT.Title = "Drink up!"
-EVENT.Description = "Everyone gets a random zombies perk"
+EVENT.Description = "Everyone gets a random COD Zombies perk bottle"
 EVENT.id = "drinkup"
+local perks = {}
+
+hook.Add("TTTPrepareRound", "RandomatDrinkupGetPerkBottleIDs", function()
+    local EQUIP_DOUBLETAP = EQUIP_DOUBLETAP or -1
+    local EQUIP_JUGGERNOG = EQUIP_JUGGERNOG or -1
+    local EQUIP_PHD = EQUIP_PHD or -1
+    local EQUIP_SPEEDCOLA = EQUIP_SPEEDCOLA or EQUIP_SPEED or -1
+    local EQUIP_STAMINUP = EQUIP_STAMINUP or -1
+
+    perks = {EQUIP_DOUBLETAP, EQUIP_JUGGERNOG, EQUIP_PHD, EQUIP_SPEEDCOLA, EQUIP_STAMINUP}
+
+    hook.Remove("TTTPrepareRound", "RandomatDrinkupGetPerkBottleIDs")
+end)
 
 function EVENT:Begin()
-    -- Gets all players...
     for k, ply in pairs(self:GetAlivePlayers()) do
-        ply:Give(table.Random(perks))
+        local perk = perks[math.random(1, #perks)]
+        ply:GiveEquipmentItem(tonumber(perk))
+        Randomat:CallShopHooks(true, perk, ply)
     end
 end
 
 function EVENT:Condition()
     local has_perk_bottles = true
 
-    for _, wep in ipairs(perks) do
-        if weapons.Get(wep) == nil then
+    for _, perk in ipairs(perks) do
+        if perk == -1 then
             has_perk_bottles = false
+            break
         end
     end
 
