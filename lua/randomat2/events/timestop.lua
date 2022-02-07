79ccbca56744264699cb468ad13514160dbde12e
The Stig <TheStig294@users.noreply.github.com>
2022-02-07 19:24:01 +1030

    Added EVENT.IsEnabled = false

--- lua/randomat2/events/timestop.lua
+++ lua/randomat2/events/timestop.lua
@@ -2,6 +2,7 @@ local EVENT = {}
 EVENT.Title = "Time Stop"
 EVENT.Description = "Freezes everything but players for 60 seconds"
 EVENT.id = "timestop"
+EVENT.IsEnabled = false
 
 hook.Add("PreRegisterSWEP", "TimeStopRandomatGetStopFunction", function(swep, class)
     if class == "weapon_ttt_timestop" then
