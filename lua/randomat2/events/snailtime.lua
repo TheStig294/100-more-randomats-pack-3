583de26fe2e251594787660abed65ee9dbe95269
The Stig <TheStig294@users.noreply.github.com>
2022-02-14 15:08:35 +1030

    Fixed snailtime convar not being in ulx menu

--- lua/randomat2/events/snailtime.lua
+++ lua/randomat2/events/snailtime.lua
@@ -114,7 +114,7 @@ end
 function EVENT:GetConVars()
     local sliders = {}
 
-    for _, v in pairs({"time"}) do
+    for _, v in pairs({"health"}) do
         local name = "randomat_" .. self.id .. "_" .. v
 
         if ConVarExists(name) then
