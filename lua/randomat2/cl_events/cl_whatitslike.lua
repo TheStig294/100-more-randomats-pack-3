fb08ff66f80842df4d007013107ff76bce51d0df
The Stig <TheStig294@users.noreply.github.com>
2022-02-25 16:14:37 +1030

    Moved missing client file to pack 1

--- lua/randomat2/cl_events/cl_whatitslike.lua
+++
@@ -1,7 +0,0 @@
-net.Receive("WhatItsLikeRandomatHideNames", function()
-    hook.Add("TTTTargetIDPlayerName", "WhatItsLikeRandomatHideNames", function(ply, client, text, clr) return false, clr end)
-end)
-
-net.Receive("WhatItsLikeRandomatEnd", function()
-    hook.Remove("TTTTargetIDPlayerName", "WhatItsLikeRandomatHideNames")
-end)
